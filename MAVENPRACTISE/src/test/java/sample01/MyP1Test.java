777b00d0f088d7277681fd2cc85849116d66998b
RAHUL-700 <satyarahul007@gmail.com>
2023-06-02 15:22:58 +0530

    browser and url

--- MAVENPRACTISE/src/test/java/sample01/MyP1Test.java
+++ MAVENPRACTISE/src/test/java/sample01/MyP1Test.java
@@ -7,22 +7,29 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class MyP1Test {
-
+ 
+	WebDriver driver;
 	@Test
 	public void m1() throws IOException {
 		/*WebDriver driver=null;
-		FileUtilities flib=new FileUtilities();
-		String browser=flib.getPropertyKeyValue("browser");
-		String url=flib.getPropertyKeyValue("url");
+		FileUtilities flib=new FileUtilities();*/
+		
+		String browser=System.getProperty("browser");
+		String url=System.getProperty("url");
 		if(browser.equalsIgnoreCase("chrome")) {
+			
+			WebDriverManager.chromedriver().setup();
 			driver=new ChromeDriver();
 		}
-		else  {
+		if(browser.equalsIgnoreCase("firefox"))  {
+			WebDriverManager.firefoxdriver().setup();
 			driver=new FirefoxDriver();
 		}
-		driver.get(url);*/
+		driver.get(url);
 		System.out.println("hi001");
 	}
 	
